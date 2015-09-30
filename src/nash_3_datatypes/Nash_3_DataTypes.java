dbea99afb3d3a18925236442d163653c35e632d1
Isaiah Nash <inash1966@stu.neisd.net>
2015-09-30 06:05:26 -0500

    DataTypes

---
+++ src/nash_3_datatypes/Nash_3_DataTypes.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nash_3_datatypes;
+
+/**
+ *
+ * @author TexasTiger32
+ */
+public class Nash_3_DataTypes {
+
+    
+    
+    public static void main(String[] args) {
+        // This is a comment
+        // We will use many comments to explain our code
+        //This program explores some of the primitive Data Types
+        int i =0;
+        float a = 0.01f;
+        double b = 0.01;
+        short c = (short).123;
+        char d1 = 123;
+        char d2 = ';';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4+"";
+        String e1 = "Isaiah Is the Best Basketball Player";
+        boolean f = true;
+        boolean g = i % 2 ==0;
+        
+        
+        
+        
+        /*
+        here we will look at the same mathematical operators
+        */
+        // + this adds two numbers and more
+        // - this subtracts two numbers and more
+        // * this multiplies two numbers and more
+        // / this divides two numbers and more
+        // % this is modulo. Shows the remainder
+        
+        
+        System.out.println(i +"an integer");
+        System.out.println(a + "a float");
+        System.out.println(b + "a double");
+        System.out.println(c + "a  short");
+        System.out.println(d1 + "a char");
+        System.out.println(d2 + "a char");
+        System.out.println(d3 + "a char");
+        System.out.println(d4 + "a char");
+        System.out.println(e + "a string");
+        System.out.println(e1 + "a string");
+        System.out.println(f + "a boolean");
+        System.out.println(g + "a boolean");
+       
+//        
+        
+        //for(i = -123;i < 123; i++) {
+//               System.out.println((char) i);
+//               System.out.println(i % 2 ==0);
+        
+        System.out.println(i + "an integer");
+        System.out.println(a + "a float");
+        System.out.println(b + "a double");
+        System.out.println(c + "a  short");
+    }
+         // a number value for char
+           // maps to a character
+        
+    
+    
+      }
+    
+}
